eab88135fb2e4aa09fce60ac878ab7e366b5ac06
Max Veytsman <maxim@ontoillogical.com>
2014-04-21 10:06:28 -0400

    Switched from jcaptcha to simple captcha, Cleanup

--- project.clj
+++ project.clj
@@ -5,11 +5,9 @@
                  [compojure "1.1.6"]
                  [org.clojure/data.zip "0.1.1"]
                  [enlive "1.1.5"]
-                 [com.octo.captcha/jcaptcha "1.0"]
                  [clj-http "0.9.0"]
-                 [clj-petfinder "0.1.0-SNAPSHOT"]]
-  ;; for com.jhlabs:imaging:jar:01012005
-  :repositories [["jahia" "http://maven.jahia.org/maven2"]]
+                 [clj-petfinder "0.1.0-SNAPSHOT"]
+                 [org.clojars.smallrivers/simplecaptcha "1.2.1"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler catcha.handler/app}
   :profiles
--- src/catcha/handler.clj
+++ src/catcha/handler.clj
@@ -1,26 +1,36 @@
 (ns catcha.handler
   (:use compojure.core)
   (:import java.io.ByteArrayInputStream
-           )
+           java.io.ByteArrayOutputStream
+           nl.captcha.Captcha
+           javax.imageio.ImageIO)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
             [catcha.captcha :as captcha]
-            [net.cgrand.enlive-html :refer [deftemplate content]]))
+            [net.cgrand.enlive-html :refer [deftemplate content set-attr]]))
 
 (deftemplate tpl-catcha "templates/catcha.html"
   [name]
-  [:.cat-name] (content name))
+  [:.cat-name] (content name)
+  [:.captcha-image :img] (set-attr :src "/api/v1/image"))
 
 (defroutes app-routes
   (route/resources "/")
   (GET "/" [] (tpl-catcha "Caty McCat"))
   (GET "/:name" [name] (tpl-catcha name))
 
-  (GET "/api/v1/blah"
+  (GET "/api/v1/image"
        []
        {:status 200
-        :headers {"Content-Type" "image/jpeg"}
-        :body (new java.io.ByteArrayInputStream (captcha/captcha-challenge-as-jpeg "blah"))})
+        :headers {"Content-Type" "image/png"}
+        :body (with-open [baos  (ByteArrayOutputStream.)]
+                (ImageIO/write (-> (doto (nl.captcha.Captcha$Builder. 176 50)
+                                     .addText)
+                                   .build
+                                   .getImage) "png" baos)
+                (.flush baos)
+                (ByteArrayInputStream. (.toByteArray baos)))
+        })
 
   (GET "/api/v1/iframe" {params :params}
        (if-let [key (:key params)]
