f17e176872ae838993a01eba4cc3b8519282a88a
Max Veytsman <maxim@ontoillogical.com>
2014-04-21 11:16:56 -0400

    Widget now shows a random cat and a captcha

--- src/catcha/handler.clj
+++ src/catcha/handler.clj
@@ -3,19 +3,35 @@
   (:import java.io.ByteArrayInputStream
            java.io.ByteArrayOutputStream
            nl.captcha.Captcha
-           javax.imageio.ImageIO)
+           javax.imageio.ImageIO
+           java.awt.Color)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
-            [net.cgrand.enlive-html :refer [deftemplate content set-attr]]))
+            [net.cgrand.enlive-html :refer [deftemplate content set-attr]]
+            [clj-petfinder.core :as pets]
+            [clojure.edn :as edn]))
+
+(def ^:dynamic *creds* (edn/read-string (slurp "config/petfinder.edn")))
 
 (deftemplate tpl-catcha "templates/catcha.html"
-  [name]
+  [id name picture]
   [:.cat-name] (content name)
-  [:.captcha-image :img] (set-attr :src "/api/v1/image"))
+  [:.captcha-image :img] (set-attr :src "/api/v1/image")
+  [:.cat-picture :img] (set-attr :src picture)
+  [:.cat-adopt :a] (set-attr :href (str "http://www.petfinder.com/petdetail/" id)))
+
+(defn random-cat
+  []
+  (pets/random-pet *creds* {:animal "cat"}))
+
+(defn serve-catcha
+  [r]
+  (let [cat (random-cat)]
+    (tpl-catcha (:id cat) (:name cat) (get-in cat [:media :photos 1 "fpm"]))))
 
 (defroutes app-routes
   (route/resources "/")
-  (GET "/" [] (tpl-catcha "Caty McCat"))
+  (GET "/" [] serve-catcha)
   (GET "/:name" [name] (tpl-catcha name))
 
   (GET "/api/v1/image"
@@ -24,7 +40,11 @@
         :headers {"Content-Type" "image/png"}
         :body (with-open [baos  (ByteArrayOutputStream.)]
                 (ImageIO/write (-> (doto (nl.captcha.Captcha$Builder. 176 50)
-                                     .addText)
+                                     .addText
+                                     .gimp
+                                     (.addBackground (nl.captcha.backgrounds.GradiatedBackgroundProducer.))
+                                     (.addNoise (nl.captcha.noise.CurvedLineNoiseProducer.))
+                                     .addBorder)
                                    .build
                                    .getImage) "png" baos)
                 (.flush baos)
